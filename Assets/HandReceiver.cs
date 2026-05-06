41f22067a106e26e1872efc3fefea686d3f9c1a2
batman <luizarthurtesch@gmail.com>
2026-05-05 22:30:30 -0300

    Add black background and hand connection lines to Unity scene
    Set camera background to solid black and draw LineRenderers between
    the 21 landmarks following MediaPipe hand connections.

--- Assets/HandReceiver.cs
+++ Assets/HandReceiver.cs
@@ -4,6 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [System.Serializable]
 public class Landmark { public float x, y, z; }
@@ -24,8 +25,43 @@ public class HandReceiver : MonoBehaviour
     public float scaleY = -8f;
     public float scaleZ = 5f;
 
+    private static readonly int[,] connections = new int[,]
+    {
+        {0,1},{1,2},{2,3},{3,4},
+        {0,5},{5,6},{6,7},{7,8},
+        {5,9},{9,10},{10,11},{11,12},
+        {9,13},{13,14},{14,15},{15,16},
+        {13,17},{17,18},{18,19},{19,20},
+        {0,17}
+    };
+
+    private LineRenderer[] lines;
+
     void Start()
     {
+        Camera.main.clearFlags = CameraClearFlags.SolidColor;
+        Camera.main.backgroundColor = Color.black;
+
+        int lineCount = connections.GetLength(0);
+        lines = new LineRenderer[lineCount];
+        for (int i = 0; i < lineCount; i++)
+        {
+            GameObject go = new GameObject($"Line_{i}");
+            go.transform.SetParent(transform);
+            LineRenderer lr = go.AddComponent<LineRenderer>();
+            lr.positionCount = 2;
+            lr.startWidth = 0.05f;
+            lr.endWidth = 0.05f;
+            lr.material = new Material(Shader.Find("Sprites/Default"));
+            lr.startColor = Color.green;
+            lr.endColor = Color.green;
+            lr.shadowCastingMode = ShadowCastingMode.Off;
+            lr.receiveShadows = false;
+            lr.useWorldSpace = true;
+            lr.enabled = false;
+            lines[i] = lr;
+        }
+
         udpClient = new UdpClient(5052);
         receiveThread = new Thread(ReceiveData);
         receiveThread.IsBackground = true;
@@ -63,6 +99,16 @@ public class HandReceiver : MonoBehaviour
                     lm.y * scaleY + scaleY / -2,
                     lm.z * scaleZ
                 );
+            }
+
+            int lineCount = connections.GetLength(0);
+            for (int i = 0; i < lineCount; i++)
+            {
+                int a = connections[i, 0];
+                int b = connections[i, 1];
+                lines[i].SetPosition(0, landmarkSpheres[a].transform.position);
+                lines[i].SetPosition(1, landmarkSpheres[b].transform.position);
+                lines[i].enabled = true;
             }
         }
     }
